916df2cbc1626ad305c85a49e7eac89e3ae0a031
Matthew Rossi <mrossi4@uncc.edu>
2020-10-07 01:46:17 -0400

    Fixed previous commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     var budget = require('./budget-data.json');
     res.json(budget);
 });
